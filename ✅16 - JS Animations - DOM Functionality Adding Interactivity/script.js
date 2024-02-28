e0d69fe5ebd80b22b66f0296e75d1be773d1eaae
Vivek Mali <148007729+its0Vivek@users.noreply.github.com>
2024-02-28 17:42:27 +0530

    DOM start

---
+++ ✅16 - JS Animations - DOM Functionality Adding Interactivity/script.js
@@ -0,0 +1,30 @@
+//  DOM
+
+
+//  DOM Manipulation
+
+//  Accessing Elements:
+
+//  document.querySelector()
+
+//  Modifying Elements:
+
+//  innerHTML, textContent
+
+//  Manipulating Styles and Classes:
+
+//  style
+
+//  classList
+
+//  Creating and Deleting Elements:
+
+//  createElement()
+
+//  appendChild()
+
+//  removeChild()
+
+//  Event Handling:
+
+//  addEventListener()
