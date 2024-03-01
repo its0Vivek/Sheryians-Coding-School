9a545ca2f3e9f24c43739031ed7545771795112b
Vivek Mali <148007729+its0Vivek@users.noreply.github.com>
2024-03-01 21:43:22 +0530

    DOM practice 7

--- ✅17 - JS Tasks - Mastering practical development challenges/practice Q6/script.js
+++ ✅17 - JS Tasks - Mastering practical development challenges/practice Q6/script.js
@@ -1,7 +1,37 @@
-let home =document.querySelector('#home')
-let about =document.querySelector('#about')
-let contact =document.querySelector('#contact')
+let home =document.querySelector('#Home')
+let about =document.querySelector('#About')
+let contact =document.querySelector('#content')
 
-let hometext =document.querySelector('#home')
-let abouttext =document.querySelector('#home')
-let contacttext =document.querySelector('#home')
+let Hometext =document.querySelector('#Hometext')
+let Abouttext =document.querySelector('#Abouttext')
+let contacttext =document.querySelector('#contenttext')
+
+
+Hometext.style.display = 'block';
+Hometext.style.width = '50%';
+
+home.addEventListener('click', function(){
+    allclear()
+    Hometext.style.display = 'block';
+    Hometext.style.width = '50%';
+})
+
+
+about.addEventListener('click', function(){
+    allclear()
+    Abouttext.style.display = 'block';
+    Abouttext.style.width = '50%';
+})
+
+contact.addEventListener('click', function(){
+    allclear()
+    contacttext.style.display = 'block';
+    contacttext.style.width = '50%';
+})
+
+
+function allclear(){
+    document.querySelectorAll('h3').forEach(function(h3){
+        h3.style.display = 'none';
+    })
+}
