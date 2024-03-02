884bbb34b3c9818334d8f8144f507de3129f428f
Vivek Mali <148007729+its0Vivek@users.noreply.github.com>
2024-03-02 18:35:51 +0530

    real life problem 2 done

---
+++ ✅18 - Real Life Developer Problems/problem 1/problem 2/Untitled-1.js
@@ -0,0 +1,38 @@
+// DEVELOPER PROBLEMS
+
+// debugging a typesError: cannit read property "x" of undefined:
+
+// let obj = {};
+
+// obj?.name?.age;
+
+//Handling Asychronous Operations
+
+// let btn = document.querySelector('button')
+
+
+// btn.addEventListener('click', function(){
+//     fetch(`https://randomuser.me/api/`)
+// .then(raw => raw.json())
+// .then(result => {
+//     const {name, email, gender, picture} = result.results[0];
+//     document.querySelector('#parent').innerHTML += `<div class="card  w-60 p-4 rounded-xl bg-zinc-800">
+//     <div class="w-20 h-20 bg-zinc-500 rounded-2xl mb-3">
+//     <img class="w-full h-full fit-cover rounded-md" src="${picture.medium}" alt=""></div>
+//     <h3 class=" font-semibold text-2xl " >${name.first} ${name.last}</h3>
+//     <h5 class=" text-sm font-semibold opacity-40">${gender}</h5>
+//     <h6 class="text-sm opacity-40" >${email}</h6>
+//     <p class="mt-5 text-xs font-semibold opacity-40">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nam vitae dolores cumque distinctio?</p>           
+// </div>`
+// })
+// })
+
+
+/////////////////////////////////////////////////////////////////////////
+
+// working wiht local storage
+
+//blocking scroll until something happens
+
+//custom tooltip
+
---
+++ ✅18 - Real Life Developer Problems/problem 1/problem 2/script.js
@@ -0,0 +1,4 @@
+document.querySelector('#block')
+.addEventListener('click', function(){
+    document.body.classList.toggle('overflow-hidden')
+})
